8108a2e32926f87d10a70c9a62c18f8fc56837e5
aaahhhrav15 <lodhaaarav12005@gmail.com>
2025-12-09 23:21:08 +0530

    added all origins to test

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,18 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Middleware
+
 app.use(cors({
-  origin: [
-    'https://retail-management-jade.vercel.app',
-    'http://localhost:5173', // Vite default port
-    'http://localhost:3000',
-    'http://localhost:3000/',
-  ],
+  origin: "*",
   methods: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
   allowedHeaders: "Content-Type, Authorization",
   credentials: true
 }));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
